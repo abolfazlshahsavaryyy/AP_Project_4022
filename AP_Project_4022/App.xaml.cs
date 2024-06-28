918c0b35c5ceccddd5cdb4f0ed253ca877a0595d
abolfazlshahsavryy <152776561+abolfazlshahsavryy@users.noreply.github.com>
2024-06-28 11:31:58 +0330

    Add project files.

---
+++ AP_Project_4022/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AP_Project_4022
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
