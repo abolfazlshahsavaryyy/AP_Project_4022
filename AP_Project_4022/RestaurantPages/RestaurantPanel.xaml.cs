dea4f08c9e29e440b51fd98d82d3b8a5b9633f80
Sepi <sep1384ehr97mani@gmail.com>
2024-07-04 00:34:57 +0330

    added restaurant panel

---
+++ AP_Project_4022/RestaurantPages/RestaurantPanel.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AP_Project_4022.RestaurantPages
+{
+    /// <summary>
+    /// Interaction logic for RestaurantPanel.xaml
+    /// </summary>
+    public partial class RestaurantPanel : Window
+    {
+        public RestaurantPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeMenu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnChangeStock_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnActiveService_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnHistory_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
