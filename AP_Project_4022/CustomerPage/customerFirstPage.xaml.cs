80db021ee1652c405b113253c2d51332c391de03
abolfazlshahsavryy <152776561+abolfazlshahsavryy@users.noreply.github.com>
2024-07-03 18:18:56 +0330

    create first page for customer

---
+++ AP_Project_4022/CustomerPage/customerFirstPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AP_Project_4022.CustomerPage
+{
+    /// <summary>
+    /// Interaction logic for customerFirstPage.xaml
+    /// </summary>
+    public partial class customerFirstPage : Window
+    {
+        public customerFirstPage()
+        {
+            InitializeComponent();
+        }
+
+        private void profileButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void searchRestaurantButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void orderhistoryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void compaintButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
