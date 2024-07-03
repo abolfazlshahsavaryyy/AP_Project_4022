931bdec7eb1f0229f00791bbb9d70091a56dfce2
Sepi <sep1384ehr97mani@gmail.com>
2024-07-04 00:04:30 +0330

    added a window to answer to complaints

---
+++ AP_Project_4022/AdminPages/CheckComplaint.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AP_Project_4022.AdminPages
+{
+    /// <summary>
+    /// Interaction logic for CheckComplaint.xaml
+    /// </summary>
+    public partial class CheckComplaint : Window
+    {
+        public CheckComplaint()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDone_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
