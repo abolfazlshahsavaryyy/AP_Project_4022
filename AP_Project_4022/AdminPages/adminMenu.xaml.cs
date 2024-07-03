f5008366c5c3c0deb40e3f0b2a7fd30aabcd2cb0
Sepi <sep1384ehr97mani@gmail.com>
2024-07-04 00:19:08 +0330

    added menu for admin

---
+++ AP_Project_4022/AdminPages/adminMenu.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AP_Project_4022.AdminPages
+{
+    /// <summary>
+    /// Interaction logic for adminMenu.xaml
+    /// </summary>
+    public partial class adminMenu : Window
+    {
+        public adminMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddRestaurant_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSearchRestaurant_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSearchComplaint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnShowUncheckedComplaints_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAnswerComplaints_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnShowAllComplaints_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
