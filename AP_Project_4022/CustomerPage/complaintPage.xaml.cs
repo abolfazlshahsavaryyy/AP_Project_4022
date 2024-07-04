e5303dbb4f3656286551be45255a7449e40f1c62
abolfazlshahsavryy <152776561+abolfazlshahsavryy@users.noreply.github.com>
2024-07-04 09:10:42 +0330

    change folder of complaint page to customer folder

--- AP_Project_4022/CustomerPage/complaintPage.xaml.cs
+++ AP_Project_4022/CustomerPage/complaintPage.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace AP_Project_4022
+namespace AP_Project_4022.CustomerPage
 {
     /// <summary>
     /// Interaction logic for complaintPage.xaml
