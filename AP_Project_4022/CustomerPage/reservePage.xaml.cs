d012dd5649f482a06bfb055fa3240f3c23e8b33b
abolfazlshahsavryy <152776561+abolfazlshahsavryy@users.noreply.github.com>
2024-07-05 21:38:15 +0330

    add resreve page to program

--- AP_Project_4022/CustomerPage/reservePage.xaml.cs
+++ AP_Project_4022/CustomerPage/reservePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AP_Project_4022.classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,7 @@ namespace AP_Project_4022.CustomerPage
     /// </summary>
     public partial class reservePage : Window
     {
+        public static int table_want = -1;
         public reservePage()
         {
             InitializeComponent();
@@ -26,6 +28,36 @@ namespace AP_Project_4022.CustomerPage
 
         private void reserveButton_Click(object sender, RoutedEventArgs e)
         {
+            
+            try
+            {
+                table_want = int.Parse(numTabelTextBox.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            if(table_want > restaurantMenuPage.Restaurant_Menu.numberTable)
+            {
+                MessageBox.Show("to much table");
+                return;
+            }
+            if(Customer.currentCustomer.SpecialService!= CustomerSpecialService.Bronze || Customer.currentCustomer.SpecialService != CustomerSpecialService.Silver || Customer.currentCustomer.SpecialService != CustomerSpecialService.Golden)
+            {
+                MessageBox.Show("only special customre can reserve");//منطق خداس
+                return;
+            }
+            if (restaurantMenuPage.Restaurant_Menu.admissionType == AdmissionType.delivery)
+            {
+                MessageBox.Show("this restuarant is not able to dine in");
+                return;
+            }
+            
+            onlinePaySimiulate ops = new onlinePaySimiulate();
+            ops.titleLabel.Content = $"reserve restaurant : {restaurantMenuPage.Restaurant_Menu.name} number table {table_want} ";
+
+            ops.Show();
 
         }
     }
