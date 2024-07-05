c34aca50ca0113d7b3e34cca97bd8b13c27f5754
Sepi <sep1384ehr97mani@gmail.com>
2024-07-05 22:07:42 +0330

    Completed Searching among restaurants part

--- AP_Project_4022/AdminPages/showSerachRestaurant.xaml.cs
+++ AP_Project_4022/AdminPages/showSerachRestaurant.xaml.cs
@@ -24,9 +24,11 @@ namespace AP_Project_4022.AdminPages
     /// </summary>
     public partial class showSerachRestaurant : Window
     {
+        string search;
         public showSerachRestaurant(string searchBy)
         {
             InitializeComponent();
+            search = searchBy;
             tbTitle.Text = "Search Restaurants by " + searchBy;
             tbHint.Text = "Enter " + searchBy;
             if (searchBy == "Complaint")
@@ -54,7 +56,75 @@ namespace AP_Project_4022.AdminPages
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-
+            if(search == "City")
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\U\source\repos\AP_Project_4022\AP_Project_4022\database.mdf;Integrated Security=True;Connect Timeout=30");
+                con.Open();
+                string command;
+                command = "select * from RestaurantTable";
+                SqlDataAdapter adapter = new SqlDataAdapter(command, con);
+                DataTable data = new DataTable();
+                adapter.Fill(data);
+                SqlCommand com = new SqlCommand(command, con);
+                com.BeginExecuteNonQuery();
+                for (int i = 0; i < data.Rows.Count; i++)
+                {
+                    if ((data.Rows[i][2].ToString().ToLower()).Contains(txtSearch.Text.ToLower()))
+                    {
+                        lstRestaurant.Items.Add(new { Username = data.Rows[i][0], City = data.Rows[i][2], Name = data.Rows[i][4], AvgPoint = data.Rows[i][6], Tables = data.Rows[i][7], Address = data.Rows[i][8], Complaints = data.Rows[i][10] });
+                    }
+                }
+                con.Close();
+            }
+            else if (search == "Name")
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\U\source\repos\AP_Project_4022\AP_Project_4022\database.mdf;Integrated Security=True;Connect Timeout=30");
+                con.Open();
+                string command;
+                command = "select * from RestaurantTable";
+                SqlDataAdapter adapter = new SqlDataAdapter(command, con);
+                DataTable data = new DataTable();
+                adapter.Fill(data);
+                SqlCommand com = new SqlCommand(command, con);
+                com.BeginExecuteNonQuery();
+                for (int i = 0; i < data.Rows.Count; i++)
+                {
+                    if ((data.Rows[i][4].ToString().ToLower()).Contains(txtSearch.Text.ToLower()))
+                    {
+                        lstRestaurant.Items.Add(new { Username = data.Rows[i][0], City = data.Rows[i][2], Name = data.Rows[i][4], AvgPoint = data.Rows[i][6], Tables = data.Rows[i][7], Address = data.Rows[i][8], Complaints = data.Rows[i][10] });
+                    }
+                }
+                con.Close();
+            }
+            else if(search == "MinScore")
+            {
+                if(!int.TryParse(txtSearch.Text, out int m) || m > 5 || m < 0)
+                {
+                    string message1 = "Please Enter valid numebr between 0 and 5!";
+                    string title1 = "Error";
+                    System.Windows.MessageBox.Show(message1, title1);
+                }
+                else
+                {
+                    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\U\source\repos\AP_Project_4022\AP_Project_4022\database.mdf;Integrated Security=True;Connect Timeout=30");
+                    con.Open();
+                    string command;
+                    command = "select * from RestaurantTable";
+                    SqlDataAdapter adapter = new SqlDataAdapter(command, con);
+                    DataTable data = new DataTable();
+                    adapter.Fill(data);
+                    SqlCommand com = new SqlCommand(command, con);
+                    com.BeginExecuteNonQuery();
+                    for (int i = 0; i < data.Rows.Count; i++)
+                    {
+                        if (double.Parse(data.Rows[i][6].ToString()) >= double.Parse(txtSearch.Text.ToString()))
+                        {
+                            lstRestaurant.Items.Add(new { Username = data.Rows[i][0], City = data.Rows[i][2], Name = data.Rows[i][4], AvgPoint = data.Rows[i][6], Tables = data.Rows[i][7], Address = data.Rows[i][8], Complaints = data.Rows[i][10] });
+                        }
+                    }
+                    con.Close();
+                }
+            }
         }
     }
 }
