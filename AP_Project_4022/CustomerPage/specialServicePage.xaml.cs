8b7d0145a53728c844cae4c4b15d6082862117a2
abolfazlshahsavryy <152776561+abolfazlshahsavryy@users.noreply.github.com>
2024-07-05 08:46:11 +0330

    add logic to specail service page

--- AP_Project_4022/CustomerPage/specialServicePage.xaml.cs
+++ AP_Project_4022/CustomerPage/specialServicePage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using AP_Project_4022.classes;
 
 namespace AP_Project_4022.CustomerPage
 {
@@ -19,9 +22,39 @@ namespace AP_Project_4022.CustomerPage
     /// </summary>
     public partial class specialServicePage : Window
     {
+        public static int check = 0;
+        
+        
         public specialServicePage()
         {
             InitializeComponent();
+        }
+
+        private void payButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(goldenRadioBox.IsChecked == false && silverRadioBox.IsChecked==false && bronzeRadioBox.IsChecked==false)
+            {
+                MessageBox.Show("Email sent successfully!","warrning",MessageBoxButton.OK,MessageBoxImage.Error);
+                return;
+            }
+            
+            if (goldenRadioBox.IsChecked == true)
+            {
+                check = 1;
+                onlinePaySimiulate ops= new onlinePaySimiulate();
+                ops.titleLabel.Content = "Golden  (300 toman) ";
+            }
+            else if( silverRadioBox.IsChecked == true )
+            {
+                check = 2;
+                onlinePaySimiulate ops = new onlinePaySimiulate();
+                ops.titleLabel.Content = "silver  (150 toman) ";
+            }
+            else { check = 3;
+                onlinePaySimiulate ops = new onlinePaySimiulate();
+                ops.titleLabel.Content = "bronze  (100 toman) ";
+            }
+
         }
     }
 }
