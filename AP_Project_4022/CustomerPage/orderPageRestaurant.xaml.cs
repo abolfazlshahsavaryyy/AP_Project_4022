ec6cf13794a64bee15c6140295885d39c3fc38cb
abolfazlshahsavryy <152776561+abolfazlshahsavryy@users.noreply.github.com>
2024-07-05 19:56:51 +0330

    add order page to food page

--- AP_Project_4022/CustomerPage/orderPageRestaurant.xaml.cs
+++ AP_Project_4022/CustomerPage/orderPageRestaurant.xaml.cs
@@ -19,9 +19,34 @@ namespace AP_Project_4022.CustomerPage
     /// </summary>
     public partial class orderPageRestaurant : Window
     {
+        public static double price;
         public orderPageRestaurant()
         {
             InitializeComponent();
+        }
+
+        private void continueButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            int number_food=0;
+            try
+            {
+                number_food = int.Parse(numberFoodTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                numberFoodTextBox.Text = string.Empty;
+                return;
+            }
+            if (foodPage.current_food.numberFood < number_food)
+            {
+
+            }
+            price=number_food*foodPage.current_food.price;
+            payPage pp=new payPage();
+            pp.Show();
+            this.Close();
         }
     }
 }
