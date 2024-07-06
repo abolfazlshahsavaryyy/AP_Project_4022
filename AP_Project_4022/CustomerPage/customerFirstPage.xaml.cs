e8e58d648d807fce4155a89a07cbfc27838b4ba1
abolfazlshahsavryy <152776561+abolfazlshahsavryy@users.noreply.github.com>
2024-07-06 07:43:10 +0330

    customize customer first page for order history

--- AP_Project_4022/CustomerPage/customerFirstPage.xaml.cs
+++ AP_Project_4022/CustomerPage/customerFirstPage.xaml.cs
@@ -42,8 +42,55 @@ namespace AP_Project_4022.CustomerPage
 
         private void orderhistoryButton_Click(object sender, RoutedEventArgs e)
         {
+            OrderHistory.allOrderHistory.Add(new OrderHistory(Customer.currentCustomer, Restaurant.allRestaurant[0]));//delete this
+            OrderHistory.allOrderHistory.Add(new OrderHistory(Customer.currentCustomer, Restaurant.allRestaurant[0]));//delete this
+            OrderHistory.allOrderHistory[0].food = new Food("food1",12,1,2,new List<Comment>(),"",new List<string>());//delete
+            OrderHistory.allOrderHistory[1].food = new Food("food2", 12, 1, 2, new List<Comment>(), "", new List<string>());//delete
             Customer.currentCustomer.order_history_page=new orderHistoryPage();
+            for(int i=0;i<OrderHistory.allOrderHistory.Count;i++)
+            {
+                if (OrderHistory.allOrderHistory[i].customer == Customer.currentCustomer)
+                {
+                    orderHistoryUserControl ohuc = new orderHistoryUserControl();
+                    ohuc.commentButton.Click += Customer.currentCustomer.order_history_page.commentButton_Click;
+                    ohuc.ratingButton.Click += Customer.currentCustomer.order_history_page.ratingButton_Click;
+                    ohuc.foodNameLabel.Content = OrderHistory.allOrderHistory[i].food.name;
+                    ohuc.restaurantLabel.Content = OrderHistory.allOrderHistory[i].restaurant.name;
+                    ohuc.Tag = OrderHistory.allOrderHistory[i].id;
+                    ohuc.ratingButton.Tag = OrderHistory.allOrderHistory[i].id;
+                    ohuc.commentButton.Tag = OrderHistory.allOrderHistory[i].id;
+                    Customer.currentCustomer.order_history_page.orderHistoryStackPanel.Children.Add(ohuc);
+                }
+
+
+            }
             Customer.currentCustomer.order_history_page.Show();
+            
+        }
+        public static void CreateStackPanelOrderHistory()
+        {
+            Customer.currentCustomer.order_history_page = new orderHistoryPage();
+            for (int i = 0; i < OrderHistory.allOrderHistory.Count; i++)
+            {
+                if (OrderHistory.allOrderHistory[i].customer == Customer.currentCustomer)
+                {
+                    orderHistoryUserControl ohuc = new orderHistoryUserControl();
+                    ohuc.commentButton.Click += Customer.currentCustomer.order_history_page.commentButton_Click;
+                    ohuc.ratingButton.Click += Customer.currentCustomer.order_history_page.ratingButton_Click;
+                    ohuc.foodNameLabel.Content = OrderHistory.allOrderHistory[i].food.name;
+                    ohuc.restaurantLabel.Content = OrderHistory.allOrderHistory[i].restaurant.name;
+                    ohuc.Tag = OrderHistory.allOrderHistory[i].id;
+                    ohuc.ratingButton.Tag = OrderHistory.allOrderHistory[i].id;
+                    ohuc.commentButton.Tag = OrderHistory.allOrderHistory[i].id;
+                    ohuc.commentOrderHistoryLabel.Content = OrderHistory.allOrderHistory[i].comment.content;
+                    ohuc.ratingButton.Content = OrderHistory.allOrderHistory[i].point;
+                    Customer.currentCustomer.order_history_page.orderHistoryStackPanel.Children.Add(ohuc);
+                }
+
+
+            }
+            Customer.currentCustomer.order_history_page.Show();
+
         }
 
         private void compaintButton_Click(object sender, RoutedEventArgs e)
