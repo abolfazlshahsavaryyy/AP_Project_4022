c7c3178b30bba200212b91ae0b81dcd106c57a0c
abolfazlshahsavryy <152776561+abolfazlshahsavryy@users.noreply.github.com>
2024-07-06 07:44:17 +0330

    add logic for rating order history page

--- AP_Project_4022/CustomerPage/orderhistoryRatingPage.xaml.cs
+++ AP_Project_4022/CustomerPage/orderhistoryRatingPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AP_Project_4022.classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Shapes;
 
 namespace AP_Project_4022.CustomerPage
@@ -19,9 +21,39 @@ namespace AP_Project_4022.CustomerPage
     /// </summary>
     public partial class orderhistoryRatingPage : Window
     {
+        public int id_comment;
         public orderhistoryRatingPage()
         {
             InitializeComponent();
+        }
+
+        private void AddCommentButyon_Click(object sender, RoutedEventArgs e)
+        {
+            if (commentContentTextBox.Text == string.Empty)
+            {
+                MessageBox.Show("rating can't be empty");
+                return;
+            }
+            try
+            {
+                int.Parse(commentContentTextBox.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("rating cant be alphabet char");
+                return;
+            }
+            for(int i = 0; i < OrderHistory.allOrderHistory.Count; i++)
+            {
+                if (OrderHistory.allOrderHistory[i].id == id_comment)
+                {
+                    OrderHistory.allOrderHistory[i].point = int.Parse(commentContentTextBox.Text);
+                    this.Close();
+                    return;
+                }
+            }
+            MessageBox.Show("unknown problem !");
+            this.Close();
         }
     }
 }
