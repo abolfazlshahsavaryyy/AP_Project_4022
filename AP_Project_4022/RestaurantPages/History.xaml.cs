9756c40e9d8918ddb492ac9113a21e33b3e9340e
Sepi <sep1384ehr97mani@gmail.com>
2024-07-07 10:29:18 +0330

    Completed getting final report of order history

--- AP_Project_4022/RestaurantPages/History.xaml.cs
+++ AP_Project_4022/RestaurantPages/History.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -205,11 +206,62 @@ namespace AP_Project_4022.RestaurantPages
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            string directoryPath = "C:\\project\\";
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            if (dataTable != null)
+                dataTable.ToCSV("C:\\project\\data.csv");
+            string message = "CSV File has been created in C:\\project!";
+            string title = "Done";
+            System.Windows.MessageBox.Show(message, title);
         }
     }
 
-    
+    public static class CSVUtlity 
+    {
+        public static void ToCSV(this DataTable dtDataTable, string strFilePath)
+        {
+            StreamWriter sw = new StreamWriter(strFilePath, false);
+            //headers
+            for (int i = 0; i < dtDataTable.Columns.Count; i++)
+            {
+                sw.Write(dtDataTable.Columns[i]);
+                if (i < dtDataTable.Columns.Count - 1)
+                {
+                    sw.Write(",");
+                }
+            }
+            sw.Write(sw.NewLine);
+            foreach (DataRow dr in dtDataTable.Rows)
+            {
+                for (int i = 0; i < dtDataTable.Columns.Count; i++)
+                {
+                    if (!Convert.IsDBNull(dr[i]))
+                    {
+                        string value = dr[i].ToString();
+                        if (value.Contains(','))
+                        {
+                            value = String.Format("\"{0}\"", value);
+                            sw.Write(value);
+                        }
+                        else
+                        {
+                            sw.Write(dr[i].ToString());
+                        }
+                    }
+                    if (i < dtDataTable.Columns.Count - 1)
+                    {
+                        sw.Write(",");
+                    }
+                }
+                sw.Write(sw.NewLine);
+            }
+            sw.Close();
+        }
+    }
+
 }
 
 
